7d016c2c6d1d8dd3a228889cd8def806891b7ddc
Eduardo <eduardofidera@gmail.com>
2018-11-30 17:40:47 -0200

    :sparkles: basic players render on home page

--- server/src/components/Home.js
+++ server/src/components/Home.js
@@ -1,11 +1,37 @@
-import React from 'react';
-import Posts from './Posts';
+import React, { Fragment } from "react"
 
-const Home = () => (
-    <div>
-        <h1>this is the home</h1>
-        <Posts />
-    </div>
-)
+export default class Home extends React.Component {
+  state = {
+    players: []
+  }
 
-export default Home
+  componentDidMount() {
+    fetch("/api/players")
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        const { data: players } = data
+        this.setState({
+          players
+        })
+      })
+  }
+
+  render() {
+    const { players } = this.state
+    return (
+      <Fragment>
+        <h1>Players</h1>
+        {players
+          ? players.map(player => (
+              <Fragment>
+                <h2>{player.name}</h2>
+                <p>level: {player.level}</p>
+              </Fragment>
+            ))
+          : "no players"}
+      </Fragment>
+    )
+  }
+}
