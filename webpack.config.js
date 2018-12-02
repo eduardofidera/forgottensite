d52d2585deaba6cf199e68798c4c87a1fab90e10
Eduardo Souza <eduardofidera@gmail.com>
2018-12-02 01:54:17 -0200

    Render CSS on server to prevent FOUC :sunglasses:

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 // entry point and output points
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const path = require('path');
 
@@ -8,6 +9,11 @@ module.exports = {
         path: path.join(__dirname, '/client/public'),
         filename: 'bundle.js'
     },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'styles.css',
+      })
+    ],
     module: {
         rules: [{
             loader: 'babel-loader',
@@ -18,13 +24,13 @@ module.exports = {
         }, {
             test: /\.s?css$/,
             use: [
-                'style-loader',
+                MiniCssExtractPlugin.loader,
                 'css-loader',
                 'sass-loader'
             ]
         }]
     },
-    devtool: 'source-map',
+    devtool: 'eval-cheap-source-map',
     devServer: {
         contentBase: path.join(__dirname, '/client/public')
     }
